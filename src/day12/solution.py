f8fa3930f3e3a4bbe761a1270ad79b0678d470a3
Cody VanZandt <codyvanzandt@wustl.edu>
2022-12-13 16:50:22 +0000

    Day 13 done

--- src/day12/solution.py
+++ src/day12/solution.py
@@ -2,27 +2,42 @@ import string
 import networkx
 from collections import defaultdict, deque
 
+
 def read_elevation_map():
     with open("src/day12/input.txt", "r") as input_file:
-        return [ list(row) for row in input_file.read().split("\n") ]
+        return [list(row) for row in input_file.read().split("\n")]
+
 
 def transform_map_to_numbers(elevation_map):
-    elevation_dictionary = { "S" : 0, "E" : 25 } | { letter : i for i, letter in enumerate(string.ascii_lowercase)}
-    return  [ [elevation_dictionary[letter] for letter in row ] for row in elevation_map ]
+    elevation_dictionary = {"S": 0, "E": 25} | {
+        letter: i for i, letter in enumerate(string.ascii_lowercase)
+    }
+    return [[elevation_dictionary[letter] for letter in row] for row in elevation_map]
+
 
 def get_edges(elevation_map):
     max_rows, max_cols = len(elevation_map), len(elevation_map[0])
     for n_row, row in enumerate(elevation_map):
         for n_col, current_node_elevation in enumerate(row):
-            adjacent_indices = ((n_row, n_col - 1), (n_row, n_col + 1), (n_row - 1, n_col), (n_row + 1, n_col))
+            adjacent_indices = (
+                (n_row, n_col - 1),
+                (n_row, n_col + 1),
+                (n_row - 1, n_col),
+                (n_row + 1, n_col),
+            )
             for (other_row, other_col) in adjacent_indices:
                 if 0 <= other_row < max_rows and 0 <= other_col < max_cols:
                     other_node_elevation = elevation_map[other_row][other_col]
                     if other_node_elevation - current_node_elevation <= 1:
-                        yield ((n_row, n_col), (other_row, other_col), )
+                        yield (
+                            (n_row, n_col),
+                            (other_row, other_col),
+                        )
+
 
 def make_graph(elevation_map):
     return networkx.DiGraph(get_edges(transform_map_to_numbers(read_elevation_map())))
+
 
 def get_start_and_end_indices(elevation_map):
     start, end = None, None
@@ -31,19 +46,20 @@ def get_start_and_end_indices(elevation_map):
             if value == "S":
                 start = (row_n, col_n)
             if value == "E":
-                end = (row_n,col_n)
+                end = (row_n, col_n)
     return start, end
 
 
 def shortest_path_length(graph, source, target):
     try:
-        return bfs(graph, source)[target] 
+        return bfs(graph, source)[target]
     except KeyError:
         return float("inf")
 
+
 def bfs(g, source):
     to_visit = deque([source])
-    distances = { source : 0 }
+    distances = {source: 0}
     while to_visit:
         new_node = to_visit.popleft()
         for neighbor in g[new_node]:
@@ -52,19 +68,32 @@ def bfs(g, source):
                 distances[neighbor] = distances[new_node] + 1
     return distances
 
+
 def part_one():
     elevation_map = read_elevation_map()
     elevation_graph = make_graph(elevation_map)
     start, end = get_start_and_end_indices(elevation_map)
     return shortest_path_length(elevation_graph, start, end)
 
+
 def part_two():
     paths = list()
     elevation_map = read_elevation_map()
     elevation_graph = make_graph(elevation_map)
     _, end = get_start_and_end_indices(elevation_map)
-    starting_points = [ (row, col) for row in range(len(elevation_map)) for col in range(len(elevation_map[0])) if elevation_map[row][col] == "a" ]
-    return min([ shortest_path_length(elevation_graph, starting_point, end) for  starting_point in starting_points ])
+    starting_points = [
+        (row, col)
+        for row in range(len(elevation_map))
+        for col in range(len(elevation_map[0]))
+        if elevation_map[row][col] == "a"
+    ]
+    return min(
+        [
+            shortest_path_length(elevation_graph, starting_point, end)
+            for starting_point in starting_points
+        ]
+    )
+
 
 print(part_one())
 print(part_two())
---
+++ src/day13/solution.py
@@ -0,0 +1,38 @@
+from collections import deque
+from functools import cmp_to_key
+
+def get_list_pairs():
+    with open("src/day13/input.txt", "r") as input_file:
+        pairs = input_file.read().split("\n\n")
+        for pair in pairs:
+            a,b = pair.split("\n")
+            yield ( (eval(a)), (eval(b)))
+
+def evaluate_pair(a, b):
+    match a, b:
+        case int(), int():
+            if a == b:
+                return None
+            else:
+                return a < b
+        case int(), list():
+            return evaluate_pair([a], b)
+        case list(), int():
+            return evaluate_pair(a, [b])
+        case list(), list():
+            for a_i, b_i in zip(a, b):
+                if (z := evaluate_pair(a_i, b_i)) is not None:
+                    return z
+            return evaluate_pair(len(a), len(b))
+
+
+def part_one():
+    pairs = get_list_pairs()
+    return sum( ( i+1 for i, (a,b) in enumerate(pairs) if evaluate_pair(a,b) ) )
+
+def part_two():
+    decode_1, decode_2 = [[2]], [[6]]
+    pairs = [packet for pair in get_list_pairs() for packet in pair] + [ decode_1, decode_2 ]
+    comparator = lambda a,b: 1 if evaluate_pair(a,b) else -1
+    sorted_packets = sorted(pairs, key=cmp_to_key(comparator), reverse=True)
+    return (sorted_packets.index(decode_1) + 1) * (sorted_packets.index(decode_2) + 1)
